8b4181e29914ef1df6128492af67d1fabcc89460
slendyG <josuegarza0608@gmail.com>
2023-01-10 00:58:51 -0800

    moved assets folder to src

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import heroImg from '../assets/heroImg.png'
+
+
+const Home = () => {
+  return (
+    <div><img src={heroImg} alt="" /></div>
+  )
+}
+
+export default Home
