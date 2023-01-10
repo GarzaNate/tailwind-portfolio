fa83eccc76d8faaf3b19803e2769c8896af10554
slendyG <josuegarza0608@gmail.com>
2023-01-10 02:02:41 -0800

    changed hero image, added home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
-import heroImg from '../assets/heroImg.png'
-
+import React from "react";
+import HeroImage from "../assets/heroImage2.jpg";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
 const Home = () => {
   return (
-    <div><img src={heroImg} alt="" /></div>
-  )
-}
+    <div name="home" className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800">
+      
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center h-full px-4 md:flex-row">
+        <div className="flex flex-col justify-center h-full">
+            <h2 className="text-4xl sm:text-7xl font-bold text-white">
+                I'm a Full Stack Developer
+            </h2>
+            <p className="text-gray-500 py-4 max-w-md">
+                I have over a year of experience building and designing software. Currently,  I love to work on web applications using technologies I like such as React, Tailwind, MongoDB, SQL, ect.
+            </p>
+            <div>
+                <button className="text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
+                    Portfolio
+                    <span className="group-hover:rotate-90 duration-300">
+                    <MdOutlineKeyboardArrowRight size={25} />
+                    </span>
+                </button>
+            </div>
+        </div>
 
-export default Home
+        <div>
+            <img src={HeroImage} alt="profile pic" className="rounded-2xl mx-auto md:w-full" style={{"width": "50%"}} />
+        </div>
+    
+      </div>
+
+    </div>
+  );
+};
+
+export default Home;
